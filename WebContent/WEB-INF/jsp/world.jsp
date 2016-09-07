f0745ed5c045bbf56e0dbf874000023d4fa821c7
heqingbao <qingbao.ho@gmail.com>
2016-09-07 23:56:08 +0800

    update

--- WebContent/WEB-INF/jsp/world.jsp
+++ WebContent/WEB-INF/jsp/world.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>Hello</h1>
+	<h1>world</h1>
 </body>
 </html>
--- src/main/java/com/lab/housing/controller/HousingController.java
+++ src/main/java/com/lab/housing/controller/HousingController.java
@@ -2,14 +2,20 @@ package com.lab.housing.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HousingController {
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
-		System.out.println("-------------------");
 		return "hello";
+	}
+
+	@RequestMapping("/world")
+	public ModelAndView world() {
+		ModelAndView view = new ModelAndView("world");
+		return view;
 	}
 
 }
