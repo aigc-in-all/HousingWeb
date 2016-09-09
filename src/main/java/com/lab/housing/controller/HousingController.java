09f6490d760a81bab09bac7b31d2ce7edb6c88de
qingbao_he <qingbao_he@kingdee.com>
2016-09-09 11:27:01 +0800

    Show all cities in home page

--- src/main/java/com/lab/housing/controller/HousingController.java
+++ src/main/java/com/lab/housing/controller/HousingController.java
@@ -1,21 +1,27 @@
 package com.lab.housing.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lab.housing.dao.CityDao;
+import com.lab.housing.model.City;
+
 @Controller
 public class HousingController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
+    @Resource
+    private CityDao cityDao;
 
-    @RequestMapping("/world")
-    public ModelAndView world() {
-        System.out.println("===========================");
-        ModelAndView view = new ModelAndView("world");
+    @RequestMapping("/")
+    public ModelAndView home() {
+        List<City> cs = cityDao.list();
+        ModelAndView view = new ModelAndView("city_list");
+        view.addObject("city_list", cs);
         return view;
     }
 
